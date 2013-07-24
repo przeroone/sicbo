c53e7fa1b653795c6be559520585ab857d8bb937
Alex Au <przeroone@gmail.com>
2013-07-24 12:46:30 +0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('sicbo', []);
+
+app.filter('reverse', function() {
+  return function(items) {
+    return items.slice().reverse();
+  };
+});
+
+function SicboCtrl($scope) {
+  $scope.results = []
+
+  $scope.addResult = function() {
+    var nr = $scope.new_result
+    $scope.results.push([parseInt(nr.charAt(0)), parseInt(nr.charAt(1)), parseInt(nr.charAt(2))]);
+    $scope.new_result = '';
+  }
+}
